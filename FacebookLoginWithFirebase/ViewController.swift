761cc651148b7f15ef6b545ab77cda0aeafd35f1
Macbook <macbook@macbooknomacbook-puro.local>
2020-12-31 13:58:47 +0900

    Initial Commit

---
+++ FacebookLoginWithFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookLoginWithFirebase
+//
+//  Created by Macbook on 2020/12/31.
+//  Copyright © 2020年 Tabata Sorane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
